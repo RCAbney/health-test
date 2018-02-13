e906c5ea97d926d7656c9408587a20a13965adf5
Ryan Abney <ryan@rcabney.com>
2018-02-12 22:19:07 -0500

    remove styling/bootstrap add survey reults

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-var buttons = document.querySelectorAll('input');
-var answerButton = document.getElementById('answerButton');
+const buttons = document.querySelectorAll('input');
+const answerButton = document.getElementById('answerButton');
 
 function getTotal() {
-var totalScore = 0;
-    for (var i = 0; i < buttons.length; i++) {
+let totalScore = 0;
+    for (let i = 0; i < buttons.length; i++) {
     if (buttons[i].checked) {
       if (buttons[i].value === 'yes') {
         totalScore++;
@@ -13,10 +13,27 @@ var totalScore = 0;
   return totalScore;
 }
 
+
+function getResults(answerTotal) {
+  const resultsP = document.querySelector('p.results');
+  let risk = '';
+  let counsel = '[schedule counsel with your PCP]';
+  if (answerTotal <= 2) {
+    risk = 'low';
+    counsel = '';
+  } else if (answerTotal > 2 && answerTotal < 5) {
+    risk = 'intermediate';
+    counsel = '[schedule counsel with your PCP]';
+  } else {
+    risk = 'high';
+    counsel = '[schedule counsel with your PCP]';
+  }
+  resultsP.textContent = `Your overall score is ${answerTotal} - ${risk} risk of OSA ${counsel}`;
+}
+
 answerButton.addEventListener('click', function(e) {
   e.preventDefault();
-  var answerTotal = getTotal();
-  console.log(`Your overall score is ${answerTotal}`);
+  const answerTotal = getTotal();
+  getResults(answerTotal);
 });
 
-//another fn to display results?
